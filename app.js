83cc81ca4621afae5542452dbd204993c33807d3
Marc Davies <marcdaviesriot@gmail.com>
2020-06-13 15:17:29 -0700

    return to previous version

--- app.js
+++ app.js
@@ -1,54 +1,69 @@
-class Data {
-  async getData(url) {
-    const response = await fetch(url);
-    const resData = await response.json();
-    return resData;
-  }
-}
+const data = [
+  {
+    name: 'Maria Salazar',
+    age: 32,
+    gender: 'female',
+    lookingFor: 'male',
+    location: 'Portland OR',
+    image: 'https://randomuser.me/api/portraits/women/12.jpg',
+  },
+  {
+    name: 'Jane Smith',
+    age: 26,
+    gender: 'female',
+    lookingFor: 'male',
+    location: 'Miami FL',
+    image: 'https://randomuser.me/api/portraits/women/15.jpg',
+  },
+  {
+    name: 'Tiffany Lauren',
+    age: 38,
+    gender: 'female',
+    lookingFor: 'female',
+    location: 'Salem OR',
+    image: 'https://randomuser.me/api/portraits/women/17.jpg',
+  },
+];
 
-//Init Data
-const data = new Data();
+const profiles = profileIterator(data);
 
-data.getData('https://jsonplaceholder.typicode.com/users').then((data) => {
-  const profiles = profileIterator(data);
-  //Creating event listener
-  document
-    .getElementById('next')
-    .addEventListener('click', nextProfile.bind(null, profiles));
-  nextProfile(profiles);
-});
+// Call first profile
+nextProfile();
 
-function nextProfile(profiles) {
-  //Getting Iteration next value
+// Next event
+document.getElementById('next').addEventListener('click', nextProfile);
+
+// Next profile display
+function nextProfile() {
   const currentProfile = profiles.next().value;
 
   if (currentProfile !== undefined) {
-    //Targeting the profile div and inputting the UI data
     document.getElementById('profile-display').innerHTML = `
-        <ul class="list-group">
-          <li class="list-group-item">Name: ${currentProfile.name}</li>
-          <li class="list-group-item">Username: ${currentProfile.username}</li>
-          <li class="list-group-item">Email: ${currentProfile.email}</li>
-          <li class="list-group-item">Company: ${currentProfile.company.name}</li>
-        </ul>
-      `;
+      <ul class="list-group">
+        <li class="list-group-item"> Name: ${currentProfile.name}</li>
+        <li class="list-group-item"> Age: ${currentProfile.age}</li>
+        <li class="list-group-item"> Preference: ${currentProfile.gender} looking for ${currentProfile.lookingFor}</li>
+      </ul>
+    `;
+
+    document.getElementById('image-display').innerHTML = `
+      <img src="${currentProfile.image}" style="width: 100%">
+    `;
   } else {
     // No more profiles
     window.location.reload();
   }
 }
 
-//Iteration function
+// Profile Iterator
 function profileIterator(profiles) {
   let nextIndex = 0;
 
   return {
     next: function () {
-      if (nextIndex < profiles.length) {
-        return { value: profiles[nextIndex++], done: false };
-      } else {
-        return { done: true };
-      }
+      return nextIndex < profiles.length
+        ? { value: profiles[nextIndex++], done: false }
+        : { done: true };
     },
   };
 }
