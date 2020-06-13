1d38feed54cb0c4f76c01f805d2d326b62bd685f
Marc Davies <marcdaviesriot@gmail.com>
2020-06-13 14:59:06 -0700

    add functionality to load and scroll through profiles

--- app.js
+++ app.js
@@ -5,22 +5,65 @@ const data = [
     gender: 'male',
     lookingFor: 'female',
     location: 'Portland OR',
-    image: 'https://randomuser.me/api/portraits/men/82.jpg'
-  }
+    image: 'https://randomuser.me/api/portraits/men/82.jpg',
+  },
   {
     name: 'Jane Smith',
     age: 26,
     gender: 'female',
     lookingFor: 'male',
     location: 'Miami FL',
-    image: 'https://randomuser.me/api/portraits/women/82.jpg'
-  }
+    image: 'https://randomuser.me/api/portraits/women/82.jpg',
+  },
   {
     name: 'William Johnson',
     age: 38,
     gender: 'male',
     lookingFor: 'female',
     location: 'Salem OR',
-    image: 'https://randomuser.me/api/portraits/men/83.jpg'
-  }
+    image: 'https://randomuser.me/api/portraits/men/83.jpg',
+  },
 ];
+
+const profiles = profileIterator(data);
+
+// Call first profile
+nextProfile();
+
+// Next event
+document.getElementById('next').addEventListener('click', nextProfile);
+
+// Next profile display
+function nextProfile() {
+  const currentProfile = profiles.next().value;
+
+  if (currentProfile !== undefined) {
+    document.getElementById('profile-display').innerHTML = `
+      <ul class="list-group">
+        <li class="list-group-item"> Name: ${currentProfile.name}</li>
+        <li class="list-group-item"> Age: ${currentProfile.age}</li>
+        <li class="list-group-item"> Preference: ${currentProfile.gender} looking for ${currentProfile.lookingFor}</li>
+      </ul>
+    `;
+
+    document.getElementById('image-display').innerHTML = `
+      <img src="${currentProfile.image}" style="width: 100%">
+    `;
+  } else {
+    // No more profiles
+    window.location.reload();
+  }
+}
+
+// Profile Iterator
+function profileIterator(profiles) {
+  let nextIndex = 0;
+
+  return {
+    next: function () {
+      return nextIndex < profiles.length
+        ? { value: profiles[nextIndex++], done: false }
+        : { done: true };
+    },
+  };
+}
