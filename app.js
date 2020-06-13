ab55b519cc7f9c4c0f367569e1d8244865e08181
Marc Davies <marcdaviesriot@gmail.com>
2020-06-13 14:39:30 -0700

    hard code sample list

---
+++ app.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    name: 'John Doe',
+    age: 32,
+    gender: 'male',
+    lookingFor: 'female',
+    location: 'Portland OR',
+    image: 'https://randomuser.me/api/portraits/men/82.jpg'
+  }
+  {
+    name: 'Jane Smith',
+    age: 26,
+    gender: 'female',
+    lookingFor: 'male',
+    location: 'Miami FL',
+    image: 'https://randomuser.me/api/portraits/women/82.jpg'
+  }
+  {
+    name: 'William Johnson',
+    age: 38,
+    gender: 'male',
+    lookingFor: 'female',
+    location: 'Salem OR',
+    image: 'https://randomuser.me/api/portraits/men/83.jpg'
+  }
+];
